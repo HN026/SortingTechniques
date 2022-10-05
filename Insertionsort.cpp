5b7edbdfdb77a1350de860a3ea72cf506eb401a4
HN026 <plancksconstant01@gmail.com>
2022-10-05 09:57:43 +0530

    commit

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -1,41 +1,34 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-
-void InsertionSort (int A[], int n)
+void InsertionSort(int A[], int n)
 {
-    int i,j,x;
-    for(i = 1; i<n; i++)
+    int i, j, x;
+    for (i = 1; i < n; i++)
     {
-         j = i-1; 
-         x = A[i];
-         while(j>-1 && A[j]>x)
-         {
-            A[j+1] = A[j];
+        j = i - 1;
+        x = A[i];
+        while (j > -1 && A[j] > x)
+        {
+            A[j + 1] = A[j];
             j--;
-         }
-         A[j+1] = x;
+        }
+        A[j + 1] = x;
     }
 }
 
-
-
-
 int main()
 {
-    int A[] = {11,13,7,2,6,9,4,5,10,3};
-    int n = sizeof(A)/sizeof(int);
+    int A[] = {11, 13, 7, 2, 6, 9, 4, 5, 10, 3};
+    int n = sizeof(A) / sizeof(int);
 
-    
-    InsertionSort(A,n);
-   
+    InsertionSort(A, n);
 
-    for( int i = 0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-     cout<<A[i]<<" ";
+        cout << A[i] << " ";
     }
-
 
     return 0;
 }
