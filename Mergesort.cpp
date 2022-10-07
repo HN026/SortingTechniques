31966372621dd1323227bc7261fd27b8aa7d9806
HuzaifaNaseer26 <huzaifa@wiseapp.live>
2022-10-07 22:26:05 +0530

    commit

---
+++ Mergesort.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+
+using namespace std;
+
+void Merge( int A[], int l, int mid, int h)
+{
+    int i = l,j=mid+1,k=l;
+    int B[100];
+    while(i<=mid && j<=h)
+    {
+        if(A[i]<A[j])
+        {
+            B[k++] = A[i++];
+        }
+        else
+        {
+            B[k++] = A[j++];
+        }
+    }
+    for(;i<=mid; i++)
+    {
+        B[k++] = A[i];
+    }
+    for(;j<=h; j++)
+    {
+        B[k++] = A[j];
+    }
+
+    for(int i=l;i<=h;i++)
+    {
+        A[i] = B[i];
+    }
+}
+
+
+void IMergeSort( int A[], int n)
+{
+    int i, p, l, h, mid ;
+    
+    for(p = 2; p<=n; p = p*2)
+    {
+        for(i=0; i+p-1; i = i+p)
+        {
+            l = i;
+            h = i+p-1;
+            mid = (l+h)/2;
+            Merge(A,l,mid,h);
+        }
+        if(n-1>p/2)
+        {
+          l = i;
+          h = i+p-1;
+          mid = (l+h)/2;
+          Merge(A,l,mid,n-1);
+        }
+    }
+
+    if(p/2<n)
+    {
+        Merge(A,0,p/2-1,n-1);
+    }
+
+}
+
+
+
+
+
+int main()
+{
+    int A[] = {11,13,7,12,16,9,24,5,10,3};
+    int n = sizeof(A)/sizeof(int);
+
+    IMergeSort(A,n);
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+
+
+    return 0;
+}
