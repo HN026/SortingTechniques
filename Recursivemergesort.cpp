5258c5a33277065ba52be170c5ceef4c6b8f5719
HN026 <plancksconstant01@gmail.com>
2022-10-07 22:51:05 +0530

    commit

--- Recursivemergesort.cpp
+++ Recursivemergesort.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
-
 int main()
 {
+     
+    int A[] = {11, 13, 7, 2, 6, 9, 4, 5, 10, 3};
+    int n = sizeof(A) / sizeof(int);
+
+
+    for(int i = 0; i< n; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+
+
     return 0;
 }
